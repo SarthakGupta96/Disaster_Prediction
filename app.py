ec79fab5abe9bf3615a775901ecdb0e22356ccbb
SarthakGupta96 <sarthakgupta270905@gmail.com>
2026-03-25 10:14:28 +0530

    Removed lstm

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import joblib
 import numpy as np
-from tensorflow.keras.models import load_model
+# from tensorflow.keras.models import load_model
 
 
 # Load data for visualization
